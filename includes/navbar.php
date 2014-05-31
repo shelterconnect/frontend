722d26abea8c28481eee897820d7e1d218341910
Ankur Jain <ankurjain@Ankurs-Mac-Pro.local>
2014-05-31 14:33:21 -0700

    Navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,10 +1,33 @@
 <style>
 .navbar{
-	
-	
+	color: #3399FF;
+	height: 40px;
+	float: left;
+	margin-bottom: 8px;
+	width: 100%;
+	font-color:;
 	
 }
 	
+.navtitle{
+	margin: 0px;
+	float: left;
+}
+
+.navlinks {
+	margin-top: 9px;
+	float: right;
+}
 </style>
 
 This is the navbar
+
+<div class="navtitle">
+    
+Navtitle    
+</div>
+
+<div class="navlinks">
+	<a id="menulink" href="./">Home</a>
+	<a id="menulink" href="./">Sign up/Log in</a>
+	
