39ab95fca6b281ab2c9320dac419bce0bb5ac61c
Manny Abascal <matmanjan@MANUELs-MacBook-Air.local>
2014-05-31 15:44:52 -0700

    pulls data from accounts, includes map icon

--- includes/mapdisplay.php
+++ includes/mapdisplay.php
@@ -10,17 +10,7 @@ window.onload = loadScript;
     
     //<![CDATA[
 var map;
-    var customIcons = {
-      green: {
-        icon: 'includes/marker_outlined_sized_green.png'
-      },
-      yellow: {
-        icon: 'includes/marker_outlined_sized_yellow.png'
-      },
-	  red: {
-        icon: 'includes/marker_outlined_sized_red.png'
-      }
-    };
+    var customIcon = "img/map_marker_icon.png"
 
     function load() {
       map = new google.maps.Map(document.getElementById("map"), {
@@ -35,39 +25,27 @@ var map;
       var infoWindow = new google.maps.InfoWindow;
 
       // Change this depending on the name of your PHP file
-      downloadUrl("engine.php", function(data) {
+      downloadUrl("https://shelterconnect.ngrok.com/organizations", function(data) {
         var xml = data.responseXML;
-        var markers = xml.documentElement.getElementsByTagName("marker");
+        var markers = xml.documentElement.getElementByTagName("marker");
         for (var i = 0; i < markers.length; i++) {
           var name = markers[i].getAttribute("name");
           var address = markers[i].getAttribute("address");
-		  var description = markers[i].getAttribute("description");
-		  var weburl = markers[i].getAttribute("weburl");
 		  if (weburl) {
 			  weburl = "<br /><a id='markerlink' href='" + weburl + "'>" + weburl + "</a>"
 			  }
-		  var freshicon = markers[i].getAttribute("freshicon");
-		  var freshnesstext
-		  if (freshicon === "green"){
-			  freshnesstext = "Fresh within 90 Minutes";
-		  } else if (freshicon === "yellow"){
-			  freshnesstext = "Fresh within 150 Minutes";
-		  } else {
-			  freshnesstext = "Not fresh. Still good food!";
-		  }
-		  var lat = markers[i].getAttribute("lat")
-		  var lng = markers[i].getAttribute("lng")
+		  var lat = markers[i].location.getAttribute("latitude")
+		  var lng = markers[i].location.getAttribute("longitude")
           var type = markers[i].getAttribute("type");
           var point = new google.maps.LatLng(
-              parseFloat(markers[i].getAttribute("lat")),
-              parseFloat(markers[i].getAttribute("lng")));
-          var html = "<div id='markerdetails'><b id='markertitle'>" + name + "</b> <br/>" + "<a id='markerlink' href='http://maps.google.com/maps?q=description+(name)+%40" + lat + "," + lng + "'>" + address + "</a>" + weburl + "<br /><br />" + description + "<br /><br /><b>"+ freshnesstext + "</b></div>";
-		  var icon = customIcons[freshicon] || {};
+              parseFloat(markers[i].location.getAttribute("lat")),
+              parseFloat(markers[i].location.getAttribute("lng")));
+          var html = "<div id='markerdetails'><b id='markertitle'>" + name + "</b> <br/>" + "<a id='markerlink' href='http://maps.google.com/maps?q=description+(name)+%40" + lat + "," + lng + "'>" + address + "</div>";
           
           var marker = new google.maps.Marker({
             map: map,
             position: point,
-            icon: icon.icon
+            icon: customIcon
           });
           bindInfoWindow(marker, map, infoWindow, html);
         }
