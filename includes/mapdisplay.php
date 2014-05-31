39ed24cb42a46e2789f9ab6cf8326cd4912db1cd
unknown <julianpoy>
2014-05-31 13:29:00 -0700

    Added mapdisplay to enable map rendering, hooked master CSS up

--- includes/mapdisplay.php
+++ includes/mapdisplay.php
@@ -1,30 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta content="yes" name="apple-mobile-web-app-capable" />
-<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="apple-touch-icon-precomposed" href="ICON HERE"/>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="icon" 
-      type="image/png" 
-      href="includes/favicon_bread_trans.png">
-<LINK href="includes/main.css" rel="stylesheet" type="text/css">
-<title>Shelter Connect Home</title>
-</head>
-<div class="navbar">
-    
-    <?php require_once("includes/navbar.php") ?>
-    
-</div>
-
-<div class="contents">
-    
-This is a test.
-<div id="map" style="height: 200px; width: 500px;"></div>
-</div>
-
-<script type="text/javascript">
+    <script type="text/javascript">
     function loadScript() {
   var script = document.createElement('script');
   script.type = 'text/javascript';
@@ -56,7 +30,7 @@ var map;
 		panControl: false,
 		mapTypeControl: false,
 		rotateControl: false,
-		streetViewControl: true
+		streetViewControl: false
       });
       var infoWindow = new google.maps.InfoWindow;
 
@@ -155,8 +129,3 @@ var map;
 
 
 
-
-
-<body>
-</body>
-</html>
