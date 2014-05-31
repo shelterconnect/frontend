d785d986e456c1f6231d726ff90ff59b65d38ded
Ankur Jain <ankurjain@Ankurs-Mac-Pro.local>
2014-05-31 15:57:13 -0700

    overlaysigninlinks

--- includes/navbar.php
+++ includes/navbar.php
@@ -28,5 +28,7 @@ Navtitle
 
 <div class="navlinks">
 	<a id="menulink" href="./">Home</a>
-	<a id="menulink" href="./">Sign up/Log in</a>
+	<a id="menulink" href="javascript: signInOverlay();">Sign in</a>
+	<a id="menulink" href="javascript: signRegOverlay();">Register</a>
+	
 	
