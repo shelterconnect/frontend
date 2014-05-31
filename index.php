adb29b092648c246f4d50e94376de050cd27896b
unknown <julianpoy>
2014-05-31 12:28:23 -0700

    Init Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="yes" name="apple-mobile-web-app-capable" />
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="apple-touch-icon-precomposed" href="ICON HERE"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="icon" 
+      type="image/png" 
+      href="includes/favicon_bread_trans.png">
+<LINK href="includes/main.css" rel="stylesheet" type="text/css">
+<title>Shelter Connect Home</title>
+</head>
+
+This is a test.  
+
+<body>
+</body>
+</html>
