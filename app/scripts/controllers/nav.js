253b0d216f4f0b5bb3de87e594305495786a97bd
Zach Latta <zach@zachlatta.com>
2014-05-31 23:18:45 -0600

    Add isActive function to NavCtrl.

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('NavCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('NavCtrl', function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+      return viewLocation === $location.path();
+    };
   });
