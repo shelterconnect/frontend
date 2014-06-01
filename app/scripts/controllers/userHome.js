528c61b8a0a81621adf6c85745c8c8874eb61906
Julian Poyourow <julianpoyo@gmail.com>
2014-06-01 13:43:32 -0700

    Styled personal user home screen.

--- app/scripts/controllers/userHome.js
+++ app/scripts/controllers/userHome.js
@@ -3,4 +3,11 @@
 angular.module('frontendApp')
   .controller('UserHomeCtrl', function ($scope, Organization) {
     $scope.me = Organization.me();
+    $scope.map = {
+      center: {
+        latitude: 39,
+        longitude: -101
+      },
+      zoom: 10
+    };
   });
