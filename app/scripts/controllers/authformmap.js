4f51fde1ccf64fb384c226b27930cd19ee20a586
Julian Poyourow <Julian@Julians-Mac-Pro.local>
2014-06-01 01:48:08 -0700

    Added google maps to login, added centering

--- app/scripts/controllers/authformmap.js
+++ app/scripts/controllers/authformmap.js
@@ -1,12 +1,7 @@
 'use strict';
 
 angular.module('frontendApp')
-  .controller('LoginCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AuthformmapCtrl', function ($scope) {
     $scope.map = {
       center: {
         latitude: 39,
