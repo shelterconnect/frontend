f7fe6fbcda45aaf8aec456459121844d35f9c96b
Zach Latta <zach@zachlatta.com>
2014-06-01 10:50:27 -0600

    Use Shelter service for /shelters/:id route.

--- app/scripts/controllers/shelter.js
+++ app/scripts/controllers/shelter.js
@@ -1,12 +1,6 @@
 'use strict';
 
-var userRoles;
-
 angular.module('frontendApp')
-  .controller('ShelterCtrl', function ($scope, $routeParams, Organization) {
-    $scope.shelter = Organization.get({id:$routeParams.id}, function (data) {
-      if (data.type !== userRoles.shelter) {
-        $scope.shelter = undefined;
-      }
-    });
+  .controller('ShelterCtrl', function ($scope, $routeParams, Shelter) {
+    $scope.shelter = Shelter.get({id:$routeParams.id});
   });
