e28b3b401e9381f4de52839abfe284411ba6dab5
Zach Latta <zach@zachlatta.com>
2014-05-31 22:52:10 -0600

    Create responsive navbar.

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('frontendApp')
+  .controller('NavCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
